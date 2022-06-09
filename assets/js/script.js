3e21b8f4e923affe78afac95c01e114db9f8c81a
Betsy <betsy.griffith12@gmail.com>
2022-06-08 22:17:03 -0400

    added styling to cards and started framework for adding the five-day-forecast api

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,7 +8,16 @@ var todaysDate = moment().format("MM/DD/YY");
 var temp = document.querySelector(".temp");
 var wind= document.querySelector(".wind");
 var humidity= document.querySelector(".humidity");
-var uV= document.querySelector(".uv-index");
+var uv= document.querySelector(".uv-index");
+
+//variables for the five day forecast
+var cardBodyOne = document.querySelector(".five-day-card-one");
+var cardBodyTwo = document.querySelector(".five-day-card-two");
+var cardBodyThree = document.querySelector(".five-day-card-three");
+var cardBodyFour = document.querySelector(".five-day-card-four");
+var cardBodyFive = document.querySelector(".five-day-card-five");
+
+
 
 $("#currentDay").append(todaysDate);
 
@@ -22,80 +31,25 @@ searchButton.addEventListener("click", function() {
           var tempValue= data['main']['temp'];
           var windValue= data['wind']['speed'];
           var humidityValue= data['main']['humidity'];
-          var uVValue= data['']
+        //   latLon= [data.coord.lat, data.coord.lon];
+        //   var uvValue= "https://api.openweathermap.org/data/2.5/onecall?lat=" + latLon[0] + "&lon=" + latLon[1] + "&appid=a0e1548f4b7fe9bc85174edebf7cf635&units=imperial";
           
           cityName.innerHTML= nameValue;
           temp.innerHTML= tempValue;
           wind.innerHTML= windValue;
           humidity.innerHTML= humidityValue;
+        //   uv.innerHTML= uvValue;
         })
-      });
+    });
+    // //get api for 5 day forecast
+    // var fiveDayForecast = "https://api.openweathermap.org/data/2.5/forecast?  "&appid=a0e1548f4b7fe9bc85174edebf7cf635&units=imperial""
+    // //then put the variable into fetch
+    // fetch()
+    //     .then(function(response) {
+    //         response.json().then(function(data) {
+    //         //add the responses for the cards
+    //     })
+    // });
 });
-      
 
-
-//my key for the API: a0e1548f4b7fe9bc85174edebf7cf635
-
-
-//fetch openWeather API
-// var getCityWeatherData = function() {
-//     var currentWeatherData = "https://api.openweathermap.org/data/2.5/weather?q=" + inputValue.value + "&appid=a0e1548f4b7fe9bc85174edebf7cf635";
-//     var forecast =
-
-//     fetch(currentWeatherData).then(function(response) {
-//         //if response is OK, then...
-//         //add city to the start api to pull the weather data for that city
-//         if(response.ok){
-//             response.json().then(function(data) { 
-//                 //the container header should have the city name, today's date, and the icon for the weather
-//                 //the container div should have the temp, wind, humidity, and UV index displayed in a list
-                
-//             });
-//             //append the container to the DOM by having a callback in the formSubmitHandler.
-//         };
-//     })
-// };
-
-
-//create another fetch and fetch the api for the five day forecast
-
-//the header on the card should be each date for the next five days
-//then there should be a weather icon
-//each card content should include the temp, wind, and humidity as a list
-//append each card to the parent container's DOM element (should happen with the getCityWeatherData callback in the form handler since this will all be one function)
-
-
-//type a city into the search form
-// var formSubmitHandler = function(event){
-//     event.preventDefault();
-
-//     var cityName = citySearchInputEl.value.trim();
-
-//     if(cityName){
-//         //when search is clicked call the openWeather API function to display the entered city's info to the DOM
-//         citySearchInputEl.value = "";
-//         getCityWeatherData();
-//     } else{
-//         alert("Please enter a city name.")
-//     }
-//     console.log(event);
-//     console.log(cityName);
-// }
-
-
-// //event listener for the search form submit button
-// citySearchFormEl.addEventListener("submit", formSubmitHandler);
-
-
-// var displayCityWeatherData = function(weatherData){
-//         console.log(weatherData);
-//         var cityName = 
-//         //create a container for each piece of data
-
-
-//         //append data to container
-
-//         //append container to the dom
-//         weatherContainerEl.appendChild(cityNameEl)
-//       };
-    
+//save to local storage
