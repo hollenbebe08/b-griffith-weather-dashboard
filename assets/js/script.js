d7c125751819ab7a2db2d61f8134c02e5b0c11ff
Betsy <betsy.griffith12@gmail.com>
2022-06-09 17:56:51 -0400

    started adding data to the cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,29 +8,30 @@ var todaysDate = moment().format("MM/DD/YY");
 var temp = document.querySelector(".temp");
 var wind= document.querySelector(".wind");
 var humidity= document.querySelector(".humidity");
-var uv= document.querySelector(".uv-index");
+var icon = document.querySelector(".weatherIcon");
+// var uv= document.querySelector(".uv-index");
 
 //variables for the five day forecast
-var cardBodyOne = document.querySelector(".five-day-card-one");
-var cardBodyTwo = document.querySelector(".five-day-card-two");
-var cardBodyThree = document.querySelector(".five-day-card-three");
-var cardBodyFour = document.querySelector(".five-day-card-four");
-var cardBodyFive = document.querySelector(".five-day-card-five");
+var cardBodyForecast = document.getElementsByClassName("five-day-card")[0];
+var forecastDate = document.querySelector(".forecastDate");
+var forecastTemp = document.querySelector(".forecastTemp");
 
-
-
-$("#currentDay").append(todaysDate);
-
+//searchButton event listener and the functions for the weather display & five day forecast
 searchButton.addEventListener("click", function() {
     event.preventDefault();
     var currentWeather = "https://api.openweathermap.org/data/2.5/weather?q=" + inputValue.value + "&appid=a0e1548f4b7fe9bc85174edebf7cf635&units=imperial"
     fetch(currentWeather)
     .then(function(response) {
         response.json().then(function(data) {
-          var nameValue= data['name'] + [todaysDate];
-          var tempValue= data['main']['temp'];
-          var windValue= data['wind']['speed'];
-          var humidityValue= data['main']['humidity'];
+          var nameValue= data['name'] + " " + "(" + [todaysDate] + ")";
+          var tempValue= ": " + data['main']['temp'] + " ℉";
+          var windValue= ": " + data['wind']['speed'] + " MPH";
+          var humidityValue= ": " + data['main']['humidity'] + "%";
+          var iconCode =  data.weather[0].icon;
+          var weatherSymbol = document.createElement("img")
+          var weatherIcon= "http://openweathermap.org/img/w/" + iconCode + ".png";
+          weatherSymbol.setAttribute("src", weatherIcon)
+          icon.append(weatherSymbol);
         //   latLon= [data.coord.lat, data.coord.lon];
         //   var uvValue= "https://api.openweathermap.org/data/2.5/onecall?lat=" + latLon[0] + "&lon=" + latLon[1] + "&appid=a0e1548f4b7fe9bc85174edebf7cf635&units=imperial";
           
@@ -42,14 +43,22 @@ searchButton.addEventListener("click", function() {
         })
     });
     // //get api for 5 day forecast
-    // var fiveDayForecast = "https://api.openweathermap.org/data/2.5/forecast?  "&appid=a0e1548f4b7fe9bc85174edebf7cf635&units=imperial""
-    // //then put the variable into fetch
-    // fetch()
-    //     .then(function(response) {
-    //         response.json().then(function(data) {
-    //         //add the responses for the cards
-    //     })
-    // });
+    var fiveDayForecast = "https://api.openweathermap.org/data/2.5/forecast?q=" + inputValue.value +  "&appid=a0e1548f4b7fe9bc85174edebf7cf635&units=imperial"
+    // then put the variable into fetch
+    fetch(fiveDayForecast)
+    // console.log(fiveDayForecast);
+    .then(function(response) {
+        response.json().then(function(data) {
+            var forecastDateValue = data.list[0].dt_txt;
+            var forecastTempValue = data.list[0].main.temp;
+            var fiveDayForecastDate = new Date(forecastDateValue).toLocaleDateString();
+
+            forecastDate.innerHTML = fiveDayForecastDate;
+            forecastTemp.innerHTML = forecastTempValue;
+        //loop through the data and add to the appropriate five day forecast card
+    // //         cardBodyForecast.forEach()
+        })
+    });
 });
 
 //save to local storage
